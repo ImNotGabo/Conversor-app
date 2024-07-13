caa8d8bb1cf6f3ce84d12666a2a55eed86ae0f07
Gabriel Duarte <gabriel.duarte18@outlook.es>
2024-07-13 18:51:59 -0400

    Add project files.

---
+++ Conversor/Views/Conversion.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Conversor.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Conversion : ContentPage
+    {
+        double cm;
+        double m;
+        public Conversion()
+        {
+            InitializeComponent();
+        }
+
+        private void Calculate()
+        {
+            cm = Convert.ToDouble(Txtcm.Text);
+            m = cm / 100;
+            LabelResult.Text = m.ToString() + "m";
+        }
+
+        private void Validate()
+        {
+            if(!string.IsNullOrEmpty(Txtcm.Text))
+            {
+                Calculate();
+            } 
+            else
+            {
+                DisplayAlert("Error", "Enter an amount", "OK");
+            }
+        }
+
+        private void BtnBack_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PopAsync();
+        }
+
+        private void BtnCalculate_Clicked(object sender, EventArgs e)
+        {
+            Validate();
+        }
+    }
+}
